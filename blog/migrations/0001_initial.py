9c5c60b438209d84ea6a20790c56837fa282acfa
saturn94 <saturn.94@mail.ru>
2016-04-28 02:53:17 +0400

    message

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Entry',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('body', models.TextField()),
+                ('slug', models.SlugField(max_length=200, unique=True)),
+                ('publish', models.BooleanField(default=True)),
+                ('created', models.DateTimeField(auto_now_add=True)),
+                ('modified', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'verbose_name': 'Blog Entry',
+                'ordering': ['-created'],
+                'verbose_name_plural': 'Blog Entries',
+            },
+        ),
+    ]
