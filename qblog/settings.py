4fafab5cb3d7283bfc15b7c2ac74e8eb3ab4ee60
saturn94 <nitrogen9876543223456789@gmail.com>
2016-04-28 04:06:07 +0400

    Update settings.py

--- qblog/settings.py
+++ qblog/settings.py
@@ -100,7 +100,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
